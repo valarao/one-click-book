d1dbafbd39c2d02cb77c431b3f2458513a72440d
Steven Z <Steven@Zhao.io>
2018-12-15 05:45:14 -0800

    Add `buildMatrix` JSDoc

--- src/helpers/matrix.js
+++ src/helpers/matrix.js
@@ -1,4 +1,22 @@
-module.exports.buildMatrix = async (page) => {
+/**
+ * Returns a matrix representation for the time/room availability
+ * on a Sauder Room Booking date-picker page.
+ *
+ * Rows represent the time.
+ * Columns represent the rooms.
+ *
+ * Example:
+ *  [0, 0, 0, 0, 0]
+ *  [1, 1, 1, 0, 0]
+ *  [1, 1, 0, 0, 0]
+ *
+ *  Rooms 0 and 1 are only free during time period 0.
+ *  Room 2 is free during time periods 0 and 2.
+ *  Rooms 3 and 4 are free during time periods 0 to 2.
+ *
+ * @param {Page} page
+ */
+async function buildMatrix(page) {
   const columns = await page.$$('#day_main > thead:nth-child(1) > tr > th');
   const rows = await page.$$('#day_main > tbody > tr');
 
@@ -20,8 +38,14 @@ module.exports.buildMatrix = async (page) => {
 
     let cursor = matrix[index].indexOf(0);
 
+    // Ignore slots with `row_label` class, which are row labels
     classes.filter(cl => cl.type !== 'row_labels').forEach((cl) => {
       const type = cl[0];
+
+      // `new` class means the time-slot is free
+      // `I*` class means the time-slot is unavailable
+      //    -> also contains info for how long
+      //       it is booked for
       if (type === 'new') {
         cursor = matrix[index].indexOf(0, cursor + 1);
       } else if (type.charAt(0) === 'I') {
@@ -37,4 +61,8 @@ module.exports.buildMatrix = async (page) => {
   });
 
   return { matrix, rows };
+}
+
+module.exports = {
+  buildMatrix,
 };
